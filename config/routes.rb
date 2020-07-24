ef933562594ce42251c83b0dfaaa7f0794261ced
Beertaster <47721626+Beertaster@users.noreply.github.com>
2020-07-23 20:35:01 -0400

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  
+  resources :categories 
+  get '/' => 'sessions#home' 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new' 
+  post '/signup' => 'users#create' 
+  delete '/logout' => 'sessions#destroy'
+  get '/most_popular' => 'restaurants#most_popular' 
+
+  get '/auth/:provider/callback' => 'sessions#create'
+ 
+  
+  resources :reviews
+  resources :restaurants do
+    resources :reviews, only: [:new, :index]
+  end
+
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
